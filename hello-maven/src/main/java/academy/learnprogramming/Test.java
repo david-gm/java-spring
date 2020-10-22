cb29e290336eb2d7c49a9fbc4ba63bbc7188db7f
david.gmeindl <david.gmeindl@gmail.com>
2020-10-22 17:21:42 +0200

    initial commit

---
+++ hello-maven/src/main/java/academy/learnprogramming/Test.java
@@ -0,0 +1,14 @@
+package academy.learnprogramming;
+
+public class Test {
+    int i = 4;
+    int j = 0;
+
+    public Test(int j) {
+        this.j = j;
+    }
+
+    public void print() {
+        System.out.println("i: " + this.i + ", j: " + this.j);
+    }
+}
